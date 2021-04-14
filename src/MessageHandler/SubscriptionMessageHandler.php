28b5da877a0cd9f90b8db234d7466b68216a27c9
Florian Pondepeyre <fpondepeyre@gmail.com>
2021-04-14 12:04:43 +0200

    step 11: add logic to handler

--- src/MessageHandler/SubscriptionMessageHandler.php
+++ src/MessageHandler/SubscriptionMessageHandler.php
@@ -2,13 +2,49 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\Subscription;
 use App\Message\SubscriptionMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class SubscriptionMessageHandler implements MessageHandlerInterface
 {
+    private $em;
+    private $logger;
+
+    public function __construct(EntityManagerInterface $em, LoggerInterface $logger)
+    {
+        $this->em = $em;
+        $this->logger = $logger;
+    }
+
     public function __invoke(SubscriptionMessage $message)
     {
-        dump($message);
+        $subscription = $this->em->getRepository(Subscription::class)->findOneBy([
+            'email' => $message->getEmail(),
+            'newsletterId' => $message->getNewsletterId()
+        ]);
+
+        if (null !== $subscription) {
+            $this->logger->info('Subscription already exist', [
+                'email' => $message->getEmail(),
+                'newsletterId' => $message->getNewsletterId()
+            ]);
+
+            return;
+        }
+
+        $subscription = new Subscription();
+        $subscription->setEmail($message->getEmail());
+        $subscription->setNewsletterId($message->getNewsletterId());
+
+        $this->em->persist($subscription);
+        $this->em->flush();
+
+        $this->logger->info('Subscription store with success', [
+            'email' => $message->getEmail(),
+            'newsletterId' => $message->getNewsletterId()
+        ]);
     }
 }
